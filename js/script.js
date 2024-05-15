a96c6531459bd616597a3f7ad9d52f18a1f84ba2
petermbachu.bincom <petermbachu.bincom@gmail.com>
2024-05-15 12:05:55 +0100

    added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const menuToggle = document.getElementById('navbar_toggle');
-  const menuList = document.querySelector('.menu_ul_items');
-  const overlay = document.getElementById('overlay');
-  
-  menuToggle.addEventListener('click', toggleMenu);
-  
+$(document).ready(function() {
+
+  // javascript to handle the scroll on the static header
+  const headerSection = $("#header");
+  const headerHeight = headerSection.outerHeight(); // Get header height
+
+  // Define the threshold for animation (in pixels)
+  const scrollThreshold = 50;
+
+  $(window).scroll(function() {
+    const scrollPosition = $(window).scrollTop();
+
+    if (scrollPosition > headerHeight) {
+      headerSection.addClass("scrolled");
+    } else {
+      headerSection.removeClass("scrolled");
+    }
+  });
+
+  // javascript to handle the menu toggle
+  const menuToggle = $("#navbar_toggle");
+  const menuList = $(".navbar_container");
+  const overlay = $("#overlay");
+
+  menuToggle.click(function() {
+    toggleMenu();
+  });
+
   function toggleMenu() {
-    menuList.classList.toggle('active');
-    overlay.classList.toggle('active');
-    menuToggle.setAttribute('aria-expanded', menuList.classList.contains('active'));
-    const menuIcon = menuToggle.querySelector('.material-icons-outlined');
-    menuIcon.textContent = menuList.classList.contains('active') ? 'close' : 'menu';
-    menuIcon.style.color = menuList.classList.contains('active') ? 'white' : '';
+    menuList.toggleClass("active");
+    overlay.toggleClass("active");
+    menuToggle.attr("aria-expanded", menuList.hasClass("active"));
+
+    const menuIcon = menuToggle.find(".material-symbols-outlined");
+    menuIcon.text(menuList.hasClass("active") ? "close" : "menu");
+    menuIcon.css("color", menuList.hasClass("active") ? "white" : "");
   }
 
 });
